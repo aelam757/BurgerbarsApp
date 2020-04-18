536f8ac95b313d663eb2764cd8f75842d3214340
Alan Elam <aelam757@gmail.com>
2020-04-18 10:53:43 -0400

    finished config

--- config/orm.js
+++ config/orm.js
@@ -40,11 +40,43 @@ let orm = {
 
         queryTable += "SET";
         queryTable += dbObj(dbObjVal);
+        queryTable += "WHERE";
+        queryTable += cond;
+
+        console.log(queryTable);
+        connection.query(queryTable, (error,res)=>{
+            if (error) throw error;
+            cd(res);
+        })
+    },
+
+    clear: function (table, cond, cb){
+        let queryTable = "DELETE FROM" + table;
+
+        queryTable = queryTable + "WHERE";
+        queryTable = queryTable + cond;
+        connection.query(queryTable, function (error, res){
+            if (error) throw error;
+            cb(res);
+        });
     }
-}
+};
 
 function dbObj(ob){
     let array = [];
 
+    for( let key in ob){
+        let value = ob[key];
+
+        if(Object.hasOwnProperty.call(ob,key)){
+            if(typeof value === "string" && value.indexOf("")>= 0){
+                value = "'"+ value + "'";
+            }
+            array.push(key + "="+ value);
+        }
+    }
+    return array.toString();
 
 }
+
+module.exports = orm;
