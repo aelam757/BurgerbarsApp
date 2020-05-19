16a6146497b5f5114116b86a73b750034e483b63
Alan Elam <aelam757@gmail.com>
2020-05-18 20:00:52 -0400

    still getting a 500 error code

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burger_db.burgers (burger_name, devoured) VALUES ('Double Cheese Burger', false);
-INSERT INTO burger_db.burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
-INSERT INTO burger_db.burgers (burger_name, devoured) VALUES ('Bison Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', true);
